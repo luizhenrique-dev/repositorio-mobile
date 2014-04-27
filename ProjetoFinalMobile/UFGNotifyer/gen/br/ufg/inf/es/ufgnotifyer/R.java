923a23eba78c34c07b24ccf24072afca680aa05c
Luiz Henrique <luizhenrique321@gmail.com>
2014-04-26 23:55:57 -0300

    Criação da NotificacaoActivity que será a tela onde será mostrada a notificação recebida

--- ProjetoFinalMobile/UFGNotifyer/gen/br/ufg/inf/es/ufgnotifyer/R.java
+++ ProjetoFinalMobile/UFGNotifyer/gen/br/ufg/inf/es/ufgnotifyer/R.java
@@ -995,7 +995,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1004,6 +1004,7 @@ containing a value of this type.
         public static final int collapseActionView=0x7f05000d;
         public static final int confSenha=0x7f050040;
         public static final int container=0x7f05003c;
+        public static final int conteudo=0x7f050042;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1011,6 +1012,7 @@ containing a value of this type.
         public static final int edit_query=0x7f050030;
         public static final int email=0x7f05003e;
         public static final int end=0x7f050013;
+        public static final int excluir=0x7f050044;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
@@ -1028,6 +1030,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int remetente=0x7f050043;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1050,6 +1053,7 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int voltar=0x7f050045;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1103,13 +1107,16 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_cadastro=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int fragment_cadastro=0x7f030019;
-        public static final int fragment_main=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int activity_notificacao=0x7f030019;
+        public static final int fragment_cadastro=0x7f03001a;
+        public static final int fragment_main=0x7f03001b;
+        public static final int fragment_notificacao=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int cadastro=0x7f0c0000;
         public static final int main=0x7f0c0001;
+        public static final int notificacao=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1156,6 +1163,7 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
         public static final int title_activity_cadastro=0x7f0a0010;
+        public static final int title_activity_notificacao=0x7f0a0011;
     }
     public static final class style {
         /** 
