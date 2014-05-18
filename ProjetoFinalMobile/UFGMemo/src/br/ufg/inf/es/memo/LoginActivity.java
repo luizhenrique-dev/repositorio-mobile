0b0963d3cabda18341da4a127c913ee596c9ea48
Luiz Henrique <luizhenrique321@gmail.com>
2014-05-17 23:46:06 -0300

    Criada a LoginActivity que irá validar as informações do usuário  e sua respectiva interface gráfica.
    Modificado também o nome do projeto (aplicativo) para UFGMemo.

---
+++ ProjetoFinalMobile/UFGMemo/src/br/ufg/inf/es/memo/LoginActivity.java
@@ -0,0 +1,67 @@
+package br.ufg.inf.es.memo;
+
+import br.ufg.inf.es.ufgnotifyer.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+		final EditText login = (EditText) findViewById(R.id.entradaLogin);
+		final EditText senha = (EditText) findViewById(R.id.entradaSenha);
+		Button entrar = (Button) findViewById(R.id.btnEntrar);
+		Button limpar = (Button) findViewById(R.id.btnLimpar);
+		Button cadastrar = (Button) findViewById(R.id.btnCadastrar);
+
+		entrar.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String strLogin = login.getText().toString();
+				String strSenha = senha.getText().toString();
+
+				if (validaLogin(strLogin, strSenha)) {
+					Toast.makeText(LoginActivity.this,
+							"Login realizado com sucesso!", Toast.LENGTH_SHORT)
+							.show();
+				} else {
+					Toast.makeText(LoginActivity.this,
+							"Erro! Login e/ou Senha estão incorretos",
+							Toast.LENGTH_SHORT).show();
+				}
+
+			}
+		});
+		
+		limpar.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				login.setText("");
+				senha.setText("");
+				login.requestFocus();
+			}
+		});
+	}
+
+	// Método que irá buscar as informações no banco de dados para validar o login
+	private boolean validaLogin(String login, String senha) {
+		Log.d("Teste", login);
+		Log.d("Teste", senha);
+		if (login.equals("LuizHenrique") && senha.equals("12345"))
+			return true;
+		else
+			return false;
+
+	}
+
+}
--- ProjetoFinalMobile/UFGNotifyer/gen/br/ufg/inf/es/ufgnotifyer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package br.ufg.inf.es.ufgnotifyer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
